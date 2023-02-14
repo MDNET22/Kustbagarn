e25151cf32d38483382e03cade9c9734e26363dc
MDNET22 <mathias.damgaard@iths.se>
2023-02-14 20:45:43 +0100

    Need to do a total overhaul on store

---
+++ site.js
@@ -0,0 +1,15 @@
+
+const products = [
+    {
+        name: 'Kanelbullar',
+        price: 35
+    },
+    {
+        name: 'Surdegsbröd vete',
+        price: 60
+    },
+    {
+        name: 'Surdegsbröd dinkel',
+        price: 70
+    }
+];
